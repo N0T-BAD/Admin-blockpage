757766ecc34c921b334e41f2c9c2c868373761e2
goeun <fuzzfuzz20@naver.com>
2023-05-13 00:26:55 +0900

    feat: 로그인 페이지 로그인 섹션과 로그인 구현중

---
+++ components/pages/login/AdminLogin.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import style from '@/components/pages/login/AdminLogin.module.css'
+
+export default function AdminLogin() {
+    return (
+        <div className={style.AdminLoginWrap}>
+
+        </div>
+    )
+}
---
+++ components/pages/login/AdminLoginSection.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import style from '@/components/pages/login/AdminLoginSection.module.css'
+
+export default function AdminLoginSection() {
+    return (
+        <section className={style.AdminLoginSection}>
+        </section>
+    )
+}
