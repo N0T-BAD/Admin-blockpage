ad5ef65bcd4009be277ce4a1d6f63e48552374d1
goeun <fuzzfuzz20@naver.com>
2023-05-15 00:45:37 +0900

    feat: layout 수정

--- components/layouts/layout.tsx
+++ components/layouts/layout.tsx
@@ -1,20 +1,12 @@
 import React, { useRouter } from "next/router"
-import Footer from "@/components/layouts/footer/Footer"
 import Header from "@/components/layouts/header/Header"
 
 export default function Layout(props: { children: React.ReactNode }) {
 
-    const router = useRouter();
     return (
         <>
             <Header />
             <div>{props.children}</div>
-            {/* {
-                router.pathname === '/' ?
-                    <Footer />
-                    :
-                    <></>
-            } */}
         </>
     )
 }
