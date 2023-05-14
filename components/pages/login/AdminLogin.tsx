ed92ecf1d1760ab6fdacf5a26c01f4ca35d5ef84
goeun <fuzzfuzz20@naver.com>
2023-05-14 22:22:30 +0900

    feat: 로그인 페이지 퍼블리싱

--- components/pages/login/AdminLogin.tsx
+++ components/pages/login/AdminLogin.tsx
@@ -1,21 +1,21 @@
 import React from 'react'
 import style from '@/components/pages/login/AdminLogin.module.css'
+import Image from 'next/image'
 
 export default function AdminLogin() {
     return (
         <div className={style.AdminLoginWrap}>
             <div className={style.AdminLogin}>
-                <h2>관리자 로그인</h2>
+                <Image src={'/assets/images/logo/logo.svg'} alt="logo" width={200} height={77} />
+                <p>관리자 페이지 입니다.</p>
                 <form>
-                    <div className={style.inputWrap}>
-                        <label htmlFor="id">아이디</label>
-                        <input type="text" id="id" />
+                    <div className={style.inputIDWrap}>
+                        <input className={style.inputID} type="text" id="id" placeholder='ID' />
                     </div>
-                    <div className={style.inputWrap}>
-                        <label htmlFor="pw">비밀번호</label>
-                        <input type="password" id="pw" />
+                    <div className={style.inputPwWrap}>
+                        <input className={style.inputPw} type="password" id="pw" placeholder='Password' />
                     </div>
-                    <button type="submit">로그인</button>
+                    <button className={style.Loginbutton} type="submit">로그인</button>
                 </form>
             </div>
         </div>
