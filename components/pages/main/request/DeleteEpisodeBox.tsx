85c52dd9bda9368b1bd8fcd4bc249526334e8a8c
goeun <fuzzfuzz20@naver.com>
2023-06-12 00:46:05 +0900

    feat: 요청 승인 페이지 페이지네이션 구현, css 수정

--- components/pages/main/request/DeleteEpisodeBox.tsx
+++ components/pages/main/request/DeleteEpisodeBox.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import style from '@/components/pages/main/request/ChangeWebtoonBox.module.css'
 import { useRouter } from 'next/router';
 import Image from 'next/image';
@@ -7,6 +7,7 @@ import { episodeStateType } from '@/types/webtoon/webtoonDataType';
 import { episodelist } from '@/state/episodelist';
 import Config from '@/configs/config.export';
 import axios from 'axios';
+import Pagination from '../../ui/pagebutton/Pagination';
 
 export default function DeleteEpisodeBox() {
 
@@ -20,6 +21,10 @@ export default function DeleteEpisodeBox() {
     router.push(`/request/${requestId}/webtoonId/${webtoonId}/episodedelete/${episodeId}/episodeNumber/${episodeNumber}`);
   }
 
+  const [limit, setLimit] = useState(10);
+  const [page, setPage] = useState(1);
+  const offset = (page - 1) * limit;
+
   useEffect(() => {
     axios.get(`${baseUrl}/webtoon-service/v1/demands?target=episode&type=remove&pageNo=0`)
       .then((res) => {
@@ -30,11 +35,11 @@ export default function DeleteEpisodeBox() {
 
   return (
     <>
-      <div className={style.adminBox}>
+      <div className={`${style.adminBox} ${episodeData.data && episodeData.data.demandView.length % 2 !== 0 ? style.adminBox2 : ''}`}>
         <div className={style.webtoontext}>
           <p>회차 삭제 요청</p>
         </div>
-        {episodeData.data && episodeData.data.demandView.map((subCategory) => (
+        {episodeData.data && episodeData.data.demandView.slice(offset, offset + limit).map((subCategory) => (
           <div className={style.webtoonInfoWrap} key={subCategory.episodeId} onClick={() => handleWebtoonClick(subCategory.webtoonId, subCategory.episodeId, subCategory.episodeNumber)}>
             <div className={style.ImgWrap}>
               <Image src={subCategory.thumbnail} alt={subCategory.episodeTitle} width={120} height={100} />
@@ -45,6 +50,14 @@ export default function DeleteEpisodeBox() {
             </div>
           </div>
         ))}
+        <footer className={style.paginationfotter}>
+          <Pagination
+            total={episodeData.data.demandView.length}
+            limit={limit}
+            page={page}
+            setPage={setPage}
+          />
+        </footer>
       </div>
     </>
   )
